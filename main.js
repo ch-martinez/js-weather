0e91314cd6940af83781bf3e2083b984f40fb465
Christian Martinez <chmartinez.cba@gmail.com>
2023-03-31 18:07:28 -0300

    add: connection with API

--- main.js
+++ main.js
@@ -1,24 +1,17 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const lat = '-33.13067'
+const lon = '-64.34992'
+const apiKey = 'cb1d7125416c813760e7c7a3ae1f3d85'
+const api_url2 = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const fetchData = async (url) => {
+    try {
+        let res = await fetch(url)
+        res = await res.json()
+        console.table(res)
+        console.table(res.weather)
+    } catch (error) {
+        console.log(error)
+    }
+}
 
-setupCounter(document.querySelector('#counter'))
+fetchData(api_url2)
