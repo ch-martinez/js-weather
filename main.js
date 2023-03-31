9e89ef07758eb7a5ae7a6b50a751a299160e556d
Christian Martinez <chmartinez.cba@gmail.com>
2023-03-31 18:24:45 -0300

    add: API connection options lang and units

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const lat = '-33.13067'
 const lon = '-64.34992'
 const apiKey = 'cb1d7125416c813760e7c7a3ae1f3d85'
-const api_url2 = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`
+const api_url2 = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&lang=es&units=Metric`
 
 const fetchData = async (url) => {
     try {
